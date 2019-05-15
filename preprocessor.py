8081f1add746d7648da6b580703517351b2021dc
Daniel Santos <dfsantosbu@unal.edu.co>
2019-05-15 14:34:21 -0500

    Improve preprocessor, fix type case.

--- preprocessor.py
+++ preprocessor.py
@@ -8,7 +8,7 @@ for line in lines:
     if( len(line)< 2 ): continue
     if( line.startswith('?')): continue
     if( line.startswith('*')):
-        category = line[1:].split(' ')[0]
+        category = line[1:].split(' ')[0].lstrip()
         cnt[category] = 0.0
     else:
         fact = line.split(',')
@@ -21,8 +21,8 @@ rd.shuffle( facts )
 for question,increase,type  in facts:
     increase = 1 if increase is '+' else -1
     fact = "question('{}', {}, '{}',{})." .format(question,
-                  1./cnt[type],
-                  type, increase)
+                  100./cnt[type],
+                  type.lower().lstrip(), increase)
     print( fact )
 
 # Create Facts 2
@@ -37,8 +37,8 @@ for line in lines:
         continue
     score, name, author, *categories = line.split(',')
     for category in categories:
-        book = "book({},'{}','{}','{}').".format(score, name, author, category)
-        relations.add((type, category))
+        book = "book({},'{}','{}','{}').".format(score, name, author, category.lower().lstrip())
+        relations.add((type.lower().lstrip(), category.lower().lstrip()))
         print(book)
 
 # Create relations
